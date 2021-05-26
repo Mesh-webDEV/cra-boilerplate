c920f0a794f9f804be32b32fe744e5efc0695c80
Mesh <msharisul696@gmail.com>
2021-05-26 03:14:47 +0300

    :sparkles: CRA boilerplate, Including: MaterialUI/[core, icons], Axios, GSAP, Materialize, TaildwindCSS, Lodash

---
+++ src/App.js
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div className='w-screen h-screen flex flex-col justify-center items-center'>
+      <div className='flex flex-col items-center'>
+        <h2> React boilerplate</h2>
+        <hr className='w-3/4 h-1 mb-4 opacity-80 bg-purple-600 border-none' />
+        <p>Tailwindcss, Material-UI, Axios, GSAP, Lodash and Materialize</p>
+        <div className='w-full h-full flex justify-end mt-4 font-semibold text-sm'>
+          - Mesh20-WEBDEV
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
